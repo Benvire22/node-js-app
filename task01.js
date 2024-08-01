3e4de91e677c3b20364bd796d0e28a3f72f5a955
Limarev Ivan <ivantaketo@gmail.com>
2024-08-01 17:32:54 +0600

    Create task01.js, install express, add .gitignore file

---
+++ task01.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const port = 8001;
+
+app.get('/:name', (req, res) => {
+    return res.send(`<h1>${req.params.name}</h1>`);
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
