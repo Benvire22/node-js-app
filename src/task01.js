88d0c8aaf626814eb99fceb4df5cc65e6a4db750
Limarev Ivan <ivantaketo@gmail.com>
2024-08-02 00:56:52 +0600

    Create task02.js, install caesar-salad, refactor code.

--- src/task01.js
+++ src/task01.js
@@ -1,7 +1,10 @@
 const express = require('express');
-
 const app = express();
-const port = 8001;
+const port = 8000;
+
+app.get('/', (req, res) => {
+    return res.send(`<h1>Enter text in url line. Example: /hello</h1>`);
+});
 
 app.get('/:name', (req, res) => {
     return res.send(`<h1>${req.params.name}</h1>`);
---
+++ src/task02.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const port = 8002;
+const app = express();
+
+const vigenere = require('caesar-salad').Vigenere;
+const password = 'password';
+
+const encodeText = (text) => vigenere.Cipher(password).crypt(text);
+const decodeText = (text) => vigenere.Decipher(password).crypt(text);
+
+app.get('/', (req, res) => {
+    return res.send(`<h1>Enter in url line. Example: /encode/hello</h1>`);
+});
+
+app.get('/encode/:text', (req, res) => {
+    const text = req.params.text;
+    const encodedText = encodeText(text);
+    return res.send(`<h1>${encodedText}</h1>`);
+});
+
+app.get('/decode/:text', (req, res) => {
+    const text = req.params.text;
+    const decodedText = decodeText(text);
+    return res.send(`<h1>${decodedText}</h1>`);
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
