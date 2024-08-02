40fc2a1115bf29842e7a6a477a6228676d522dd9
Limarev Ivan <ivantaketo@gmail.com>
2024-08-02 18:42:25 +0600

    Add .editorconfig file, refactor code

--- src/task01.js
+++ src/task01.js
@@ -3,13 +3,13 @@ const app = express();
 const port = 8000;
 
 app.get('/', (req, res) => {
-    return res.send(`<h1>Enter text in url line. Example: /hello</h1>`);
+  return res.send(`<h1>Enter some text in search line. Example: /hello</h1>`);
 });
 
 app.get('/:name', (req, res) => {
-    return res.send(`<h1>${req.params.name}</h1>`);
+  return res.send(`<h1>${req.params.name}</h1>`);
 });
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
--- src/task02.js
+++ src/task02.js
@@ -1,29 +1,29 @@
 const express = require('express');
-const port = 8002;
 const app = express();
+const port = 8002;
 
 const vigenere = require('caesar-salad').Vigenere;
-const password = 'password';
+const password = 'HelloWorld2024!';
 
 const encodeText = (text) => vigenere.Cipher(password).crypt(text);
 const decodeText = (text) => vigenere.Decipher(password).crypt(text);
 
 app.get('/', (req, res) => {
-    return res.send(`<h1>Enter in url line. Example: /encode/hello</h1>`);
+  return res.send(`<h1>Enter some text in search line. Example: /encode/hello</h1>`);
 });
 
 app.get('/encode/:text', (req, res) => {
-    const text = req.params.text;
-    const encodedText = encodeText(text);
-    return res.send(`<h1>${encodedText}</h1>`);
+  const text = req.params.text;
+  const encodedText = encodeText(text);
+  return res.send(`<h1>${encodedText}</h1>`);
 });
 
 app.get('/decode/:text', (req, res) => {
-    const text = req.params.text;
-    const decodedText = decodeText(text);
-    return res.send(`<h1>${decodedText}</h1>`);
+  const text = req.params.text;
+  const decodedText = decodeText(text);
+  return res.send(`<h1>${decodedText}</h1>`);
 });
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
